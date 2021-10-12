12dce21bc5036778c2b7c7ad979e8abe909d2fb1
MR3730415 <m3n7m3.97.415@gmail.com>
2021-10-12 14:58:57 +0000

    [Update]5章

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,27 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
+    @blog = Blog.new
+  end
+  
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path(blog.i)
   end
 
   def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title,:caetgry,:body)
   end
 end
