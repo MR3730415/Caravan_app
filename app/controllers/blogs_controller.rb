9eaaa6b98d370d3622f5b2ad61cc8f215d7d21c3
MR3730415 <m3n7m3.97.415@gmail.com>
2021-10-13 14:02:21 +0000

    [Finishi]CARAVAN完成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    # 記事を全件取得
     @blogs = Blog.all
   end
 
@@ -12,16 +13,32 @@ class BlogsController < ApplicationController
   end
   
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path(blog.i)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
-
+  
   def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
   
   private
   def blog_params
-    params.require(:blog).permit(:title,:caetgry,:body)
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+   validates :title, presence: true
+  validates :category, presence: true
+  validates :body, presence: true
 end
